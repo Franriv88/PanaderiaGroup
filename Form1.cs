c6cd96836392c11af745dadfd0797dfc2fab087f
Juanche <juan.bernabo@davinci.edu.com.ar>
2023-05-10 01:51:01 -0300

    updating form1 to use as default

--- Form1.cs
+++ Form1.cs
@@ -8,19 +8,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Proyecto_Panadería
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+namespace Proyecto_Panadería {
+	public partial class Form1 : Form {
+		//views declaration
+		Login login;
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            RegistroCliente r = new RegistroCliente();
-            r.Show();
-        }
-    }
+		public Form1() {
+			InitializeComponent();
+			this.login = new Login();
+			this.login.MdiParent = this;
+			this.login.TransfEvento = this.TransfDelegadoHome;
+			this.login.Show()
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			RegistroCliente r = new RegistroCliente();
+			r.Show();
+		}
+
+		public void TransfDelegadoHome() {
+			this.login.Close();
+			this.home = new Home();
+		}
+	}
 }

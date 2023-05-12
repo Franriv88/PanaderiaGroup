1e046c9415f4ee1f6f8439c0eebad63790e5ba02
JuanBer <juan.bernabo@wearepsh.com>
2023-05-12 01:28:25 -0300

    PanaderiaSystem work in progress

--- PanaderiaSystem.cs
+++ PanaderiaSystem.cs
@@ -2,6 +2,25 @@ using System;
 
 namespace Proyecto_Panadería {
 	public class PanaderiaSystem {
+		public List<User> users;
+		public List<Product> products;
+		public AccessDB db;
+
+		public PanaderiaSystem() {
+			this.users = new List<User>();
+			this.products = new List<Product>();
+			this.db = new AccessDB();
+			this.attributesInitialize();
+		}
+
+		private void attributesInitialize() {
+			this.users = db.userInitialize();
+			this.products = db.productInitialize();
+		}
+
+		public bool addProduct() {
+			//TODO: Define whats the thing that we need in the product class
+		}
 
 	}
 }

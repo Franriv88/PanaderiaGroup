ef008f27bf3dede02606ce267115d0c44d2cbfbb
JuanBer <juan.bernabo@wearepsh.com>
2023-05-12 01:28:37 -0300

    user class implemented

---
+++ User.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Proyecto_Panadería {
+	public class User {
+		public int id { get; set; }
+		public string name { get; set; }
+		public string lastName { get; set; }
+		public string userName { get; set; }
+		public string password { get; set; }
+		public bool isAdmin { get; set; }
+		public bool isDeleted { get; set; }
+
+		public User() {}
+
+		public User(int id, string name, string lastName, string userName, string password) {
+			this.id = id;
+			this.name = name;
+			this.lastName = lastName;
+			this.userName = userName;
+			this.isAdmin = false;
+			this.isDeleted = false;
+		}
+
+		public User(int id, string name, string lastName, string userName, string password, bool isAdmin, bool isDeleted) {
+			this.id = id;
+			this.name = name;
+			this.lastName = lastName;
+			this.userName = userName;
+			this.isAdmin = isAdmin;
+			this.isDeleted = isDeleted;
+		}
+	}
+}

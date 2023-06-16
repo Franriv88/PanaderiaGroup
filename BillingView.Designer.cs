a996e00db8e3cc24345576bd2fc98f2dfc89483a
Francisco Rivas <francisco.rivas@davinci.edu.ar>
2023-06-16 03:02:31 -0300

    Ajustes mínimos de imágen

--- BillingView.Designer.cs
+++ BillingView.Designer.cs
@@ -217,7 +217,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Mistral", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(203, 49);
+            this.label4.Location = new System.Drawing.Point(214, 47);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(110, 44);
             this.label4.TabIndex = 15;
